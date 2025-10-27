acda712fe365fa226d09c01a2474d0a48b2c29da
Kenton Knelle <kken51915@gmail.com>
2025-10-27 09:07:42 -0400

    Exercise 2 Push

---
+++ PalindromeExercise/PalindromeExercise/WordSmith.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Data;
+
+namespace PalindromeExercise;
+
+public class WordSmith
+{
+    public bool IsPalindrome(string word)
+    {
+        var reverse = "";
+
+        for (int i = word.Length - 1; i >= 0; i--)
+        {
+            reverse += word[i];
+        }
+
+        if (reverse.ToLower() == word.ToLower())
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+        
+    }
+}
--- PalindromeExercise/PalindromeExerciseTests/UnitTest1.cs
+++ PalindromeExercise/PalindromeExerciseTests/UnitTest1.cs
@@ -1,14 +1,33 @@
 using System;
+using PalindromeExercise;
 using Xunit;
 
 namespace PalindromeExerciseTests
 {
     public class UnitTest1
     {
-        [Fact]
-        public void Test1()
+        [Theory]
+        [InlineData("peep", true)]
+        [InlineData("other", false)]
+        public void IsAPalindrome(string word, bool expected)
         {
+            
+            //Arrange
+
+            var input = new WordSmith();
+
+            //Act
+            
+            bool actual = input.IsPalindrome(word);
+            
+
+            //Assert
+            
+            Assert.Equal(expected, actual);
+
+
 
         }
+      
     }
 }
